39dbc7e65dac2aded91d7288c43472010c014114
dpattison3 <dpattison3@gatech.edu>
2016-08-13 19:01:45 -0400

    added deployment configuration files and utilities

---
+++ TAEN_APP/management/commands/createTalents.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from TAEN_APP.models import Talent
+ 
+  
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not Talent.objects.filter(talent=0).exists():
+            Talent.objects.create(talent=0)
+        if not Talent.objects.filter(talent=1).exists():
+            Talent.objects.create(talent=1)
+        if not Talent.objects.filter(talent=2).exists():
+            Talent.objects.create(talent=2)
+        if not Talent.objects.filter(talent=3).exists():
+            Talent.objects.create(talent=3)
