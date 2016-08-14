9ab98cf7905e7a7273c678103fbf8953cb743419
dpattison3 <dpattison3@gatech.edu>
2016-08-13 21:16:50 -0400

    added if statement for setting debug mode in settings - strings aren't booleans

--- TAEN/settings.py
+++ TAEN/settings.py
@@ -19,7 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+if os.environ['DEBUG'] == 'False':
+    DEBUG = False
+else:
+    DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = []
