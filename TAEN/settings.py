0e0dfd1cc66e5ad35d7e21129a0d354b43653a34
dpattison3 <dpattison3@gatech.edu>
2016-08-14 11:53:10 -0400

    changed styling of image upload form inputs

--- TAEN/settings.py
+++ TAEN/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ['DEBUG'] == 'False':
+if 'DEBUG' in os.environ and os.environ['DEBUG'] == 'False':
     DEBUG = False
 else:
     DEBUG = True
